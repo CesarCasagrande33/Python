6ae3420ec239f6e91e1b579e44feb15b0c44b969
Cesar Casagrande <60935076+Cesarcasagrande@users.noreply.github.com>
2023-08-09 19:38:55 -0300

    At
    Atualização

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python
  Estudos e exercicios da linguagem Python
+
+ # Aulas de 01 a 03
+ Exemplificando: Strings(str), Tipos (Int, Floot, Boolean). 
---
+++ aulas/aula01.py
@@ -0,0 +1,19 @@
+# Permite escrever comentario com uma linha
+
+print("Hello, Word!")
+
+# Argumento nomeado sep=(separador)
+# \n = Quebra de linha
+print(12, 34, sep="-")
+print(56, 78, end="\n")
+
+# Python = Linguagem de programação
+# Tipo de tipagem = Dinâmica / Forte
+# str -> string -> texto
+# Strings são textos que estão dentro de aspas
+
+# Aspas simples
+print('Cesar Casagrande')
+
+# Aspas dupla
+print("Cesar Casagrande")
